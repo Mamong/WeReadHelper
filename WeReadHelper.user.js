9e2fdddd9113d79bc196e5c3495c2869233e7dd8
Mamong <chyy.meng@gmail.com>
2021-10-29 18:23:31 +0800

    Update WeReadHelper.user.js

--- WeReadHelper.user.js
+++ WeReadHelper.user.js
@@ -20,6 +20,20 @@
     //Chrome&IE
     console.log("onmousewheel")
     window.onmousewheel = document.onmousewheel = wheel;
+    
+    //统一处理滚轮滚动事件
+    function wheel(event) {
+        var delta = 0;
+        if (!event) event = window.event;
+        if (event.wheelDelta) {//IE、chrome浏览器使用的是wheelDelta，并且值为“正负120”
+            delta = event.wheelDelta / 120;
+            if (window.opera) delta = -delta;//因为IE、chrome等向下滚动是负值，FF是正值，为了处理一致性，在此取反处理
+        } else if (event.detail) {//FF浏览器使用的是detail,其值为“正负3”
+            delta = -event.detail / 3;
+        }
+        if (delta)
+            handle(delta);
+    }
 
     //上下滚动时的具体处理函数
     function handle(delta) {
